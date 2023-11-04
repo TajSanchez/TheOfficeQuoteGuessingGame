5b4de9cd0506de23d5551de99a00b452b9cf4401
Taj <tajmarie92@gmail.com>
2023-11-03 22:51:48 -0500

    Add project files.

---
+++ TheOfficeQuoteGuessingGameDesktop/Form1.cs
@@ -0,0 +1,10 @@
+namespace TheOfficeQuoteGuessingGameDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
